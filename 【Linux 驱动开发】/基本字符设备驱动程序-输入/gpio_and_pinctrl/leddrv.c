471ba31823664db672ea8f38b36994e677ba9a41
Staok <superxhy@qq.com>
2022-04-23 18:37:41 +0800

    更新内容、更新目录

---
+++ 【Linux 驱动开发】/基本字符设备驱动程序-输入/gpio_and_pinctrl/leddrv.c
@@ -0,0 +1,166 @@
+#include <linux/module.h>
+#include <linux/platform_device.h>
+
+#include <linux/fs.h>
+#include <linux/errno.h>
+#include <linux/miscdevice.h>
+#include <linux/kernel.h>
+#include <linux/major.h>
+#include <linux/mutex.h>
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+#include <linux/stat.h>
+#include <linux/init.h>
+#include <linux/device.h>
+#include <linux/tty.h>
+#include <linux/kmod.h>
+#include <linux/gfp.h>
+#include <linux/gpio/consumer.h>
+#include <linux/of.h>
+
+
+/* 1. 确定主设备号                                                                 */
+static int major = 0;
+static struct class *led_class;
+static struct gpio_desc *led_gpio;
+
+
+/* 3. 实现对应的open/read/write等函数，填入file_operations结构�?                  */
+static ssize_t led_drv_read (struct file *file, char __user *buf, size_t size, loff_t *offset)
+{
+	printk("%s %s line %d\n", __FILE__, __FUNCTION__, __LINE__);
+	return 0;
+}
+
+/* write(fd, &val, 1); */
+static ssize_t led_drv_write (struct file *file, const char __user *buf, size_t size, loff_t *offset)
+{
+	int err;
+	char status;
+	//struct inode *inode = file_inode(file);
+	//int minor = iminor(inode);
+	
+	printk("%s %s line %d\n", __FILE__, __FUNCTION__, __LINE__);
+	err = copy_from_user(&status, buf, 1);
+
+	/* 根据次设备号和status控制LED */
+	gpiod_set_value(led_gpio, status);
+	
+	return 1;
+}
+
+static int led_drv_open (struct inode *node, struct file *file)
+{
+	//int minor = iminor(node);
+	
+	printk("%s %s line %d\n", __FILE__, __FUNCTION__, __LINE__);
+	/* 根据次设备号初始化LED */
+	gpiod_direction_output(led_gpio, 0);
+	
+	return 0;
+}
+
+static int led_drv_close (struct inode *node, struct file *file)
+{
+	printk("%s %s line %d\n", __FILE__, __FUNCTION__, __LINE__);
+	return 0;
+}
+
+/* 定义自己的file_operations结构�?                                             */
+static struct file_operations led_drv = {
+	.owner	 = THIS_MODULE,
+	.open    = led_drv_open,
+	.read    = led_drv_read,
+	.write   = led_drv_write,
+	.release = led_drv_close,
+};
+
+/* 4. 从platform_device获得GPIO
+ *    把file_operations结构体告诉内核：注册驱动程序
+ */
+static int chip_demo_gpio_probe(struct platform_device *pdev)
+{
+	//int err;
+	
+	printk("%s %s line %d\n", __FILE__, __FUNCTION__, __LINE__);
+
+	/* 4.1 设备树中定义�? led-gpios=<...>;	*/
+    led_gpio = gpiod_get(&pdev->dev, "led", 0);
+	if (IS_ERR(led_gpio)) {
+		dev_err(&pdev->dev, "Failed to get GPIO for led\n");
+		return PTR_ERR(led_gpio);
+	}
+    
+	/* 4.2 注册file_operations 	*/
+	major = register_chrdev(0, "100ask_led", &led_drv);  /* /dev/led */
+
+	led_class = class_create(THIS_MODULE, "100ask_led_class");
+	if (IS_ERR(led_class)) {
+		printk("%s %s line %d\n", __FILE__, __FUNCTION__, __LINE__);
+		unregister_chrdev(major, "led");
+		gpiod_put(led_gpio);
+		return PTR_ERR(led_class);
+	}
+
+	device_create(led_class, NULL, MKDEV(major, 0), NULL, "100ask_led%d", 0); /* /dev/100ask_led0 */
+        
+    return 0;
+    
+}
+
+static int chip_demo_gpio_remove(struct platform_device *pdev)
+{
+	device_destroy(led_class, MKDEV(major, 0));
+	class_destroy(led_class);
+	unregister_chrdev(major, "100ask_led");
+	gpiod_put(led_gpio);
+    
+    return 0;
+}
+
+
+static const struct of_device_id ask100_leds[] = {
+    { .compatible = "100ask,leddrv" },
+    { },
+};
+
+/* 1. 定义platform_driver */
+static struct platform_driver chip_demo_gpio_driver = {
+    .probe      = chip_demo_gpio_probe,
+    .remove     = chip_demo_gpio_remove,
+    .driver     = {
+        .name   = "100ask_led",
+        .of_match_table = ask100_leds,
+    },
+};
+
+/* 2. 在入口函数注册platform_driver */
+static int __init led_init(void)
+{
+    int err;
+    
+	printk("%s %s line %d\n", __FILE__, __FUNCTION__, __LINE__);
+	
+    err = platform_driver_register(&chip_demo_gpio_driver); 
+	
+	return err;
+}
+
+/* 3. 有入口函数就应该有出口函数：卸载驱动程序时，就会去调用这个出口函�? *     卸载platform_driver
+ */
+static void __exit led_exit(void)
+{
+	printk("%s %s line %d\n", __FILE__, __FUNCTION__, __LINE__);
+
+    platform_driver_unregister(&chip_demo_gpio_driver);
+}
+
+
+/* 7. 其他完善：提供设备信息，自动创建设备节点                                     */
+
+module_init(led_init);
+module_exit(led_exit);
+
+MODULE_LICENSE("GPL");
+
+
---
+++ 【Linux 驱动开发】/基本字符设备驱动程序-输入/查询方式-非阻塞/02_button_drv_for_boards/06_button_drv_for_100ask_stm32mp157/board_100ask_stm32mp157.c
@@ -0,0 +1,118 @@
+﻿#include <linux/module.h>
+
+#include <linux/fs.h>
+#include <linux/io.h>
+#include <linux/errno.h>
+#include <linux/miscdevice.h>
+#include <linux/kernel.h>
+#include <linux/major.h>
+#include <linux/mutex.h>
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+#include <linux/stat.h>
+#include <linux/init.h>
+#include <linux/device.h>
+#include <linux/tty.h>
+#include <linux/kmod.h>
+#include <linux/gfp.h>
+#include <asm/io.h>
+
+#include "button_drv.h"
+
+
+struct stm32mp157_gpio {
+  volatile unsigned int MODER;    /*!< GPIO port mode register,               Address offset: 0x00      */
+  volatile unsigned int OTYPER;   /*!< GPIO port output type register,        Address offset: 0x04      */
+  volatile unsigned int OSPEEDR;  /*!< GPIO port output speed register,       Address offset: 0x08      */
+  volatile unsigned int PUPDR;    /*!< GPIO port pull-up/pull-down register,  Address offset: 0x0C      */
+  volatile unsigned int IDR;      /*!< GPIO port input data register,         Address offset: 0x10      */
+  volatile unsigned int ODR;      /*!< GPIO port output data register,        Address offset: 0x14      */
+  volatile unsigned int BSRR;     /*!< GPIO port bit set/reset,               Address offset: 0x18      */
+  volatile unsigned int LCKR;     /*!< GPIO port configuration lock register, Address offset: 0x1C      */
+  volatile unsigned int AFR[2];   /*!< GPIO alternate function registers,     Address offset: 0x20-0x24 */
+} ;
+
+
+/* RCC_PLL4CR */
+static volatile unsigned int *RCC_PLL4CR; 
+
+/* RCC_MP_AHB4ENSETR */
+static volatile unsigned int *RCC_MP_AHB4ENSETR; 
+
+static struct stm32mp157_gpio *gpiog; /* KEY1: PG3, KEY2: PG2 */
+
+static void board_stm32mp157_button_init (int which) /* 初始化button, which-哪个button */      
+{
+    if (!RCC_PLL4CR)
+    {
+        RCC_PLL4CR = ioremap(0x50000000 + 0x894, 4);
+        RCC_MP_AHB4ENSETR = ioremap(0x50000000 + 0xA28, 4);
+
+        gpiog = ioremap(0x50008000, sizeof(struct stm32mp157_gpio));
+    }
+
+    if (which == 0)
+    {
+        /* 1. enable PLL4 
+         * CG15, b[31:30] = 0b11
+         */
+		*RCC_PLL4CR |= (1<<0);
+		while((*RCC_PLL4CR & (1<<1)) == 0);
+
+		/* 2. enable GPIOG */
+		*RCC_MP_AHB4ENSETR |= (1<<6);
+		
+		/* 3. 设置PG3为GPIO模式, 输入模式 
+		 */
+		gpiog->MODER &= ~(3<<6);
+        
+    }
+    else if(which == 1)
+    {
+        /* 1. enable PLL4 
+         * CG15, b[31:30] = 0b11
+         */
+		*RCC_PLL4CR |= (1<<0);
+		while((*RCC_PLL4CR & (1<<1)) == 0);
+
+		/* 2. enable GPIOG */
+		*RCC_MP_AHB4ENSETR |= (1<<6);
+		
+		/* 3. 设置PG2为GPIO模式, 输入模式 
+		 */
+		gpiog->MODER &= ~(3<<4);
+    }
+    
+}
+
+static int board_stm32mp157_button_read (int which) /* 读button, which-哪个 */
+{
+    //printk("%s %s line %d, button %d, 0x%x\n", __FILE__, __FUNCTION__, __LINE__, which, *GPIO1_DATAIN);
+    if (which == 0)
+        return (gpiog->IDR & (1<<3)) ? 1 : 0;
+    else
+        return (gpiog->IDR & (1<<2)) ? 1 : 0;
+}
+    
+static struct button_operations my_buttons_ops = {
+    .count = 2,
+    .init = board_stm32mp157_button_init,
+    .read = board_stm32mp157_button_read,
+};
+
+int board_stm32mp157_button_drv_init(void)
+{
+    register_button_operations(&my_buttons_ops);
+    return 0;
+}
+
+void board_stm32mp157_button_drv_exit(void)
+{
+    unregister_button_operations();
+}
+
+module_init(board_stm32mp157_button_drv_init);
+module_exit(board_stm32mp157_button_drv_exit);
+
+MODULE_LICENSE("GPL");
+
